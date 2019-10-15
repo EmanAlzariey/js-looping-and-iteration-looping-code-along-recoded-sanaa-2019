c54db2ccfab6777b72b44e33f6e7eaa63b328c58
Eman Al-zariey <eman_alzariey@yahoo.com>
2019-10-15 15:59:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solutions in this file
+
+function writeCards(["Ada", "Brendan", "Ali"], "birthday"); {
+for(let i=0; i<writeCards.length; i++){
+  console.log(`"Thank you, ${writeCards[i]}, for the wonderful birthday gift!`);
+}
+return writeCards;
+}
