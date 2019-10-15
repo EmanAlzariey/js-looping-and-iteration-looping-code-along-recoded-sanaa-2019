de7efc8806824d289476a950d8762ff8cebe4ea3
Eman Al-zariey <eman_alzariey@yahoo.com>
2019-10-15 16:08:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 // Code your solutions in this file
 
 
-// const name = ["Ada", "Brendan", "Ali"]; 
+// const name = ["Ada", "Brendan", "Ali"];
 // const event = ["birthday"];
 
 function writeCards(name , event) {
 
       let thankYou =[] //Collect the messages in an array
-  
+
   for (let i = 0; i < name.length; i++) {
     // console.log(`Thank you, ${name[i]}, for the wonderful ${event} gift! `);  // dont have to display
     thankYou.push(`Thank you, ${name[i]}, for the wonderful ${event} gift! `); // push to the new array that is been display
   }
-   
 
-  return thankYou; //and and returning the new array. 
+
+  return thankYou; //and and returning the new array.
 }
-
-
-
